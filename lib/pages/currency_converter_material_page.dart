3776a66c96a472df077aed8f002a4a717c46d74c
Mirza <113027159+M1rzoni@users.noreply.github.com>
2026-05-11 15:20:38 +0200

    UI

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -6,16 +6,48 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
 
 @override
 Widget build(BuildContext context){
-  return const Scaffold(
+  return Scaffold(
+    backgroundColor: Color.fromRGBO(0, 0, 0, 1),
           body: Center(child: 
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-            Text('0')
-          ],
+           const Text('0',
+            style: TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(255, 255, 255, 1)
+              ),
+            ),
+            TextField(
+                style: TextStyle(
+                  color: Colors.black
+                ),
+                decoration: InputDecoration(
+                hintText: 'Please enter the amount in USD',
+                hintStyle: TextStyle(
+                  color: Colors.black
+                ),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 2.0,
+                    style: BorderStyle.solid,
+                    strokeAlign: BorderSide.strokeAlignCenter
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                 
                 )
-          ,) 
-  );
-}
+              )
+            ),
+          ],
+        )
+      ) 
+    );
+  }
 }
