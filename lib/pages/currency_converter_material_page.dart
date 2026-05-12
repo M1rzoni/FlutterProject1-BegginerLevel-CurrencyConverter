0be977338ae53909198bf14bf670584061dbb87e
Mirza <113027159+M1rzoni@users.noreply.github.com>
2026-05-12 15:18:38 +0200

    okey

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -1,53 +1,54 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget{
-
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
+  @override
+  State<CurrencyConverterMaterialPage> createState() => _CurrencyConverterMaterialPage();
+}
 
+class _CurrencyConverterMaterialPage extends State<CurrencyConverterMaterialPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
 
+class CurrencyConverterMaterialPagee extends StatelessWidget {
+  const CurrencyConverterMaterialPagee({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    double result = 0;
+    final TextEditingController textEditingController = TextEditingController();
 
-@override
-Widget build(BuildContext context){
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(width: 2.0, style: BorderStyle.solid),
+      borderRadius: BorderRadius.circular(5),
+    );
 
-int result = 0;
-final TextEditingController textEditingController = TextEditingController();
-
-
-final border = OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    width: 2.0,
-                    style: BorderStyle.solid,
-                  ),
-                  borderRadius: BorderRadius.circular(5)
-                );
-
-  return Scaffold(
-    backgroundColor: Color.fromRGBO(0, 0, 0, 1),
-          body: Center(child: 
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-           const Text('0',
-            style: TextStyle(
-              fontSize: 45,
-              fontWeight: FontWeight.bold,
-              color: Color.fromRGBO(255, 255, 255, 1)
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              result.toString(),
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(255, 255, 255, 1),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(10),
               child: TextField(
                 controller: textEditingController,
-                  style: TextStyle(
-                    color: Colors.black
-                  ),
-                  decoration: InputDecoration(
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
                   hintText: 'Please enter the amount in USD',
-                  hintStyle: TextStyle(
-                    color: Colors.black
-                  ),
+                  hintStyle: TextStyle(color: Colors.black),
                   prefixIcon: Icon(Icons.monetization_on_outlined),
                   prefixIconColor: Colors.black,
                   filled: true,
@@ -57,31 +58,32 @@ final border = OutlineInputBorder(
                       color: Colors.red,
                       width: 2.0,
                       style: BorderStyle.solid,
-                      strokeAlign: BorderSide.strokeAlignCenter
+                      strokeAlign: BorderSide.strokeAlignCenter,
                     ),
                     borderRadius: BorderRadius.circular(5),
                   ),
                   enabledBorder: border,
                 ),
                 keyboardType: const TextInputType.numberWithOptions(
-                  decimal: true
-                )
+                  decimal: true,
+                ),
               ),
             ),
 
-            TextButton(onPressed: () {
-              debugPrint('Button clicked');
-            }, 
-           style: const ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll<Color>(Colors.white),
-            foregroundColor: WidgetStatePropertyAll(Colors.black),
-            fixedSize: WidgetStatePropertyAll(Size(250, 30))
-           ),
-           child: Text('Convert'),
-            )
+            TextButton(
+              onPressed: () {
+                result = double.parse(textEditingController.text) * 81;
+              },
+              style: const ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll<Color>(Colors.white),
+                foregroundColor: WidgetStatePropertyAll(Colors.black),
+                fixedSize: WidgetStatePropertyAll(Size(250, 30)),
+              ),
+              child: Text('Convert'),
+            ),
           ],
-        )
-      ) 
+        ),
+      ),
     );
   }
 }
